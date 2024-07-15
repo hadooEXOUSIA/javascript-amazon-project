5b7126967f00939c1ab4201242de5ddf357f5e6e
ExousiaDev <maghami.2000@gmail.com>
2024-07-15 23:47:44 +0330

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
-let productsHTML= '';
+let productsHTML = "";
 
 products.forEach((product) => {
-        productsHTML+=`<div class="product-container">
+  productsHTML += `<div class="product-container">
          <div class="product-image-container">
                 <img
                 class="product-image"
@@ -18,10 +18,14 @@ products.forEach((product) => {
                 class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars * 10}.png"
                 />
-                <div class="product-rating-count link-primary">${product.rating.count}</div>
+                <div class="product-rating-count link-primary">${
+                  product.rating.count
+                }</div>
             </div>
 
-            <div class="product-price">$${(product.priceCents/100).toFixed(2)}</div>
+            <div class="product-price">$${(product.priceCents / 100).toFixed(
+              2
+            )}</div>
 
             <div class="product-quantity-container">
                 <select>
@@ -45,11 +49,13 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">Add to Cart</button>
+            <button class="add-to-cart-button button-primary js-add-to-cart">Add to Cart</button>
         </div>`;
+});
 
-})
-
-console.log(productsHTML);
-
-document.querySelector('.js-product-grid').innerHTML = productsHTML;
+document.querySelector(".js-product-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(`event listener!!`);
+  });
+});
