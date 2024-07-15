e9628bb425a2012ee6d743258988a808186009c3
ExousiaDev <maghami.2000@gmail.com>
2024-07-15 23:48:44 +0330

    git

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,13 +49,30 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart">Add to Cart</button>
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">Add to Cart</button>
         </div>`;
 });
 
 document.querySelector(".js-product-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`event listener!!`);
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
   });
 });
