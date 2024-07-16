0d46654af4f3fa76007ab39f5cfd5ab648fe08cb
ExousiaDev <maghami.2000@gmail.com>
2024-07-16 19:44:45 +0330

    Delete products from HTML ehrn clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem) => {
   });
   console.log(cartItem.quantity);
   cartSummerHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -94,5 +95,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
