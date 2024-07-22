2fb3b0d9c1aea1d437009f6488904f8e9447f128
ExousiaDev <maghami.2000@gmail.com>
2024-07-22 13:10:48 +0330

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,2 +1,4 @@
 import { renderOrderSummery } from "./checkout/orderSummery.js";
+import { renderPaymentSummery } from "./checkout/paymentSummery.js";
 renderOrderSummery();
+renderPaymentSummery();
--- scripts/checkout/orderSummery.js
+++ scripts/checkout/orderSummery.js
@@ -1,26 +1,21 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utilities/moeny.js";
-import { deliveryOptions } from "../../data/deliveryOption.js";
+import {
+  deliveryOptions,
+  getDeliveryOption,
+} from "../../data/deliveryOption.js";
+import { renderPaymentSummery } from "./paymentSummery.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 export function renderOrderSummery() {
   let cartSummerHTML = ``;
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
-    let matchingProduct;
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
-    const deliveryOptionId = cartItem.deliveryOptionId;
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const matchingProduct = getProduct(productId);
+
+    let deliveryOptionId = cartItem.deliveryOptionId;
+    let deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
@@ -120,6 +115,7 @@ export function renderOrderSummery() {
       const deliveryOptionId = element.dataset.deliveryOptionId;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummery();
+      renderPaymentSummery();
     });
   });
 }
